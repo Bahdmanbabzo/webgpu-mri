0b91e2ddfa14f80c717d24f59c66a047ac923b05
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2025-06-06 20:44:56 +0100

    Make typed array based on datatype from array buffer

--- src/utils/niiLoader.js
+++ src/utils/niiLoader.js
@@ -1,6 +1,8 @@
 import * as nifti from 'nifti-reader-js';
 
-export default async function loadNiftiFile(filePath) {
+export async function loadNiftiFile(filePath) {
+
+    let voxelData; 
 
     const response = await fetch(filePath); 
     if (!response.ok) {
@@ -19,9 +21,53 @@ export default async function loadNiftiFile(filePath) {
     const header = nifti.readHeader(arrayBuffer);
     const image = nifti.readImage(header, arrayBuffer);
     
+    // Make typed array depending on dataType
+    switch(header.datatypeCode) {
+        case 2: 
+            voxelData = new Uint8Array(image);
+            break;
+        case 4:
+            voxelData = new Int16Array(image);
+            break;
+        case 8:
+            voxelData = new Int32Array(image);
+            break;
+        case 16:
+            voxelData = new Float32Array(image);
+            break;
+    }
     console.log('NIfTI Header:', header);
     console.log('Image dimensions:', header.dims);
     console.log('Data type:', header.datatypeCode);
     console.log('Image data:', image);
+    console.log('Voxel data:', voxelData);
+
+    return {
+        header: header,
+        image: image,
+    }
   }
+}
+
+export function processNiftiData(header) {
+    const dims = header.dims; 
+    const pixelDims = header.pixDims; 
+
+    const voxelSpacing = { 
+        x: pixelDims[1],
+        y: pixelDims[2],
+        z: pixelDims[3]
+    }; 
+
+    const physicalSize = { 
+        x: dims[1] * voxelSpacing.x,
+        y: dims[2] * voxelSpacing.y,
+        z: dims[3] * voxelSpacing.z
+    }
+
+    return {
+        voxelSpacing: voxelSpacing,
+        physicalSize: physicalSize,
+        isIsotropic: voxelSpacing.x === voxelSpacing.y && voxelSpacing.y === voxelSpacing.z,
+    }
 }
