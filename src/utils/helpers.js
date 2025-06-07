4e06adf5364e97c1862754abbcf373d5aa9164a6
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2025-06-07 11:19:50 +0100

    Moved helpers to the same class.

---
+++ src/utils/helpers.js
@@ -0,0 +1,77 @@
+import * as nifti from 'nifti-reader-js';
+export default class Helpers{
+    static async loadNiftiFile(filePath) {
+    
+        let voxelData; 
+    
+        const response = await fetch(filePath); 
+        if (!response.ok) {
+            throw new Error(`Failed to fetch NIfTI file: ${response.statusText}`);
+        }
+    
+        let arrayBuffer = await response.arrayBuffer();
+    
+        if (nifti.isCompressed(arrayBuffer)) {
+            const decompressed = nifti.decompress(arrayBuffer);
+            console.log('Decompressed NIfTI data');
+            arrayBuffer = decompressed;
+        }
+    
+        if (nifti.isNIFTI(arrayBuffer)) {
+        const header = nifti.readHeader(arrayBuffer);
+        const image = nifti.readImage(header, arrayBuffer);
+        
+        // Make typed array depending on dataType
+        switch(header.datatypeCode) {
+            case 2: 
+                voxelData = new Uint8Array(image);
+                break;
+            case 4:
+                voxelData = new Int16Array(image);
+                break;
+            case 8:
+                voxelData = new Int32Array(image);
+                break;
+            case 16:
+                voxelData = new Float32Array(image);
+                break;
+        }
+        console.log("This is voxel buffer", voxelData);
+        console.log("This is the header", header);
+        return {
+            header: header,
+            image: image,
+            voxelData: voxelData
+        }
+      }
+    }
+    
+    static processNiftiData(header) {
+        const dims = header.dims; 
+        const pixelDims = header.pixDims; 
+    
+        const voxelSpacing = { 
+            x: pixelDims[1],
+            y: pixelDims[2],
+            z: pixelDims[3]
+        }; 
+    
+        const physicalSize = { 
+            x: dims[1] * voxelSpacing.x,
+            y: dims[2] * voxelSpacing.y,
+            z: dims[3] * voxelSpacing.z
+        }
+    
+        return {
+            voxelSpacing: voxelSpacing,
+            physicalSize: physicalSize,
+            isIsotropic: voxelSpacing.x === voxelSpacing.y && voxelSpacing.y === voxelSpacing.z,
+        }
+    }; 
+
+    static alignToWebGPU(val, alignment) {
+        return Math.floor((val + alignment - 1) / alignment) * alignment;
+    }; 
+
+
+}
