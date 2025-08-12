109719688e5cc278138532b9bbad653fde744c02
unknown <oserebameh.beckley210710032@st.lasu.edu.ng>
2025-08-12 18:06:01 +0100

    Plotting works.

--- src/viewer_scripts/main.py
+++ src/viewer_scripts/main.py
@@ -6,10 +6,17 @@ import nibabel as nib
 
 # Load with nibabel
 im = nib.load('public/sub-001/anat/sub-001_T1w.nii.gz')
-print('Data type:', im.dtype)
-print('Min. value:', im.min())
-print('Max value:', im.max())
+print('Stored dtype:', im.header.get_data_dtype())
+# Perfect for visualisation
+vol = im.get_fdata(dtype=np.float32)
+print('Volume shape:', vol.shape, 'dtype:', vol.dtype)
+print('Min. value:', vol.min())
+print('Max. value:', vol.max())
 
 # Plot the grayscale image
-plt.imshow(im, cmap='gray', vmin=0, vmax=255)
-plt.colorbar()
+mid = vol.shape[2] // 2
+front = vol.shape[0] // 2
+plt.imshow(vol[:, :, mid], cmap='gray', vmin=vol.min(), vmax=vol.max())
+plt.axis('off')
+plt.title('Middle slice')
+plt.show()
